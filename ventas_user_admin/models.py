4bf78767b2ce5f4b19dfe825f9c0b486d3480790
SrBuj <juanpbujaldon@gmail.com>
2025-05-06 19:26:05 -0300

    migrations

--- ventas_user_admin/models.py
+++ ventas_user_admin/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.conf import settings
+
 
 class User(models.Model):
-    nombre = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    contraseña = models.CharField(max_length=255)
-    telefono = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
