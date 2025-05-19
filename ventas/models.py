56e19c64b78d6958be9dbbb850426d60d77453f1
SrBuj <juanpbujaldon@gmail.com>
2025-05-19 18:04:27 -0300

    .

--- ventas/models.py
+++ ventas/models.py
@@ -10,7 +10,6 @@ class User(AbstractUser):
     def __str__(self):
         return self.nombre
 
-
 class Admin(models.Model):
     nombre = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
